c592279d6aa592804ed0b8b2ffad0514138ad988
Nash <knshetty@live.com>
2015-09-03 23:58:58 +0300

    Refactor-Ember: Amend the environment variables to build for production

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/emberjs-svg-examples/';
+    ENV.baseURL = '/emberjs-svg-examples';
   }
 
   return ENV;
